5989fea8f2da5accaceafe1f5966d202b9cd3fca
Valeria Esquivel <valeria.esquivel@grupocassa.com>
2023-01-07 22:39:49 -0600

    070123-W01 Task of part 1 - 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,88 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("");
+        Console.Write("Write your percentage grade: ");
+        
+        string gradeFromUser = Console.ReadLine();
+        float grade = float.Parse(gradeFromUser);
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+
+            if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+
+            if (grade % 10 < 3)
+            {
+                sign = "-";
+            } 
+            else if (grade % 10 > 7)
+            {
+                sign = "+";
+            } 
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+
+            if (grade % 10 < 3)
+            {
+                sign = "-";
+            } 
+            else if (grade % 10 > 7)
+            {
+                sign = "+";
+            } 
+            else
+            {
+                sign = "";
+            }
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+
+            if (grade % 10 < 3)
+            {
+                sign = "-";
+            } 
+            else if (grade % 10 > 7)
+            {
+                sign = "+";
+            } 
+            else
+            {
+                sign = "";
+            }
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"{letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed. Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You failed! Try hard the next time!");
+        }
     }
 }
